50d8de60e7a9bcace51e7b0a6eb7c6fc5675ab2e
Adithya Krishna <adithyaakrz@gmail.com>
2025-07-22 19:55:21 +0530

    Added context files

--- src/Contexts/TodoContext.js
+++ src/Contexts/TodoContext.js
@@ -7,4 +7,16 @@ export const TodoContext = createContext({
         completed: false
     }
     ],
+    addTodo: (todo) => {},
+    updateTodo: (id, updatedTodo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
 });
+
+export const useTodo = () => {
+    
+    return useContext(TodoContext);}
+    //use of this custom hook is that it allows us to access the TodoContext without needing to import useContext every time.
+
+
+export const TodoProvider = TodoContext.Provider;
