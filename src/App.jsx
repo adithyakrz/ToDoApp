0b2ac2dd8d23cb33a3bcc2b1f750c974811edeba
Adithya Krishna <adithyaakrz@gmail.com>
2025-08-02 08:46:37 +0530

    added todo functions in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import { TodoProvider } from './Contexts/TodoContext';
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] =useState([])
+
+  const addTodo = (todo) => {
+    // Ensure the todo has an id and is added to the beginning of the list
+    // Using Date.now() to generate a unique id
+    setTodos((prev) > [{id: Date.now, ...todo} , ...prev])
+  }
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => {
+      prevTodo.id === todo.id ? todo : prevTodo
+    }))
+  }
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id))
+  }
+
+  const toggleComplete = (id) => {
+    setTodos((prev) => 
+      prev.map((prevTodo) => 
+        prevTodo.id === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo
+    //spread operator is used to create a new object with the updated completed status while keeping the other properties unchanged.
+      )
+    )
+    }
 
   return (
-    <>
-    
-    </>
+    <TodoProvider value = {{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
+      <h1>Test</h1>
+    </TodoProvider>
   )
 }
 
